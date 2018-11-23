a60534d752710abcdcaca52f3d8613dc3a9bc91d
Adrian Klingen <adrian@tamtam.nl>
2018-11-23 14:16:38 +0100

    Removed plugins

--- gulpfile.js/tasks/script/webpack-plugins.js
+++ gulpfile.js/tasks/script/webpack-plugins.js
@@ -5,28 +5,7 @@ const UglifyJsPlugin    = requireCached('uglifyjs-webpack-plugin');
 
 const plugins = [];
 
-plugins.push( new webpack.optimize.CommonsChunkPlugin({
-    names: ['main', 'main-es'], // Names have to be equal to entry files of Webpack
-    minChunks: 2,
-    children: true,
-    deepChildren: true
-}) );
-
 if (config.minify) {
-
-    plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
-    // plugins.push(new webpack.NoEmitOnErrorsPlugin());
-    // plugins.push(new UglifyJsPlugin({
-    //     cache: true,
-    //     parallel: true,
-    //     uglifyOptions: {
-    //         keep_classnames: true,
-    //         keep_fnames: true,
-    //         mangle: {
-    //             safari10: true
-    //         }
-    //     }
-    // }));
 
 }
 
