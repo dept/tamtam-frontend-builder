63df16f2886991cc4908e636535eb24f8cac7fd1
Adrian Klingen <adrian@tamtam.nl>
2019-05-03 14:00:22 +0200

    Migrated sw-precache to workbox-build

--- gulpfile.js/tasks/sw.js
+++ gulpfile.js/tasks/sw.js
@@ -3,19 +3,39 @@ const config = require('../config');
 const path = require('path');
 
 const gulp = requireCached('gulp');
-const swPrecache = requireCached('sw-precache');
+const workboxBuild = requireCached('workbox-build');
 
 /**
  *  Gulp task for compiling serviceworker
  */
-gulp.task('sw', function (callback) {
+gulp.task('sw', function(callback) {
+    const swDest = path.join(
+        path.resolve(config.projectDirectory, config.dest.getPath('sw')),
+        'sw.js'
+    );
 
-    swPrecache.write(path.join(path.resolve(config.projectDirectory, config.dest.getPath('sw')), 'sw.js'), {
-        directoryIndex: false,
-        staticFileGlobs: [config.source.getFileGlobs('sw')],
-        stripPrefix: config.source.sw.strip,
-        runtimeCaching: config.source.sw.runtimeCaching,
-        navigateFallback: '/?utm_source=homescreen'
-    }, callback);
+    const swConfig = config.source.sw;
 
+    // Added for backwards compatibility
+    if (swConfig.files) delete swConfig.files;
+    if (swConfig.strip) delete swConfig.strip;
+    if (swConfig.path) {
+        if (!swConfig.globDirectory) swConfig.globDirectory = swConfig.path;
+        delete swConfig.path;
+    }
+
+    workboxBuild
+        .generateSW({
+            swDest,
+            navigateFallback: '/?utm_source=homescreen',
+            importScripts: ['/assets/sw-extend.js'],
+            ...swConfig
+        })
+        .then(({ count, size }) => {
+            const sizeInMB = (size / Math.pow(1024, 2)).toFixed(2);
+            console.log(
+                `Generated ${swDest}, which will precache ${count} files, totaling ${sizeInMB} MB.`
+            );
+            callback();
+        });
 });
