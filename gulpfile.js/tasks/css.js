8867047ccb86432607c47852af4010620198a8ab
Adrian Klingen <adrian@tamtam.nl>
2019-10-25 10:58:40 +0200

    Added overrideBrowserlist

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -35,7 +35,7 @@ gulp.task('css', function () {
         // @see: http://caniuse.com/
         // @see: https://github.com/postcss/autoprefixer-core
         autoprefixer: {
-            browsers: ['last 3 versions', 'iOS 8', 'IE 11'],
+            overrideBrowserslist: ['iOS 8', 'IE 11'],
             grid: true,
             remove: true // By default, Autoprefixer will not only add new prefixes, but also remove outdated ones.
         },
