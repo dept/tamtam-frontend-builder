a5664e97774c0d1caedc758b35038b2a5e68d576
Adrian Klingen <adrian@tamtam.nl>
2019-10-25 10:56:32 +0200

    Updated packages

--- gulpfile.js/tasks/script/eslint-config.js
+++ gulpfile.js/tasks/script/eslint-config.js
@@ -1,6 +1,14 @@
 module.exports = esLintConfig = {
-    enforce: 'pre',
-    test: /\.js?$/,
-    loader: require.resolve('eslint-loader'),
-    exclude: /node_modules/
-}
+  enforce: 'pre',
+  test: /\.js?$/,
+  exclude: /node_modules/,
+  use: [
+    {
+      loader: require.resolve('eslint-loader'),
+      loader: 'eslint-loader',
+      options: {
+        formatter: require('eslint/lib/cli-engine/formatters/stylish')
+      }
+    }
+  ]
+};
