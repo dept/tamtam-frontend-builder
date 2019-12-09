2aec7daaf9adb1c1e33810f665a8b77c52f0f1d0
Adrian Klingen <adrian@tamtam.nl>
2019-12-09 10:18:07 +0100

    Fix eslint

--- utils/jest-transformer.js
+++ utils/jest-transformer.js
@@ -1,5 +1,7 @@
 const webpackConfig = require('../gulpfile.js/tasks/script/webpack-config')
+
 webpackConfig.legacyConfig.module.rules[1].use.options.presets[0][1].modules = 'commonjs'
+webpackConfig.legacyConfig.module.rules[1].use.options.presets.push('@babel/preset-typescript')
 
 module.exports = require('babel-jest').createTransformer(
   webpackConfig.legacyConfig.module.rules[1].use.options,
