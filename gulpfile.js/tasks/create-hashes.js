5e130ffc1074a59ded583e3eee37053a757dbdde
Adrian Klingen <adrian@tamtam.nl>
2020-03-26 23:27:15 +0100

    Fix missing rev json

--- gulpfile.js/tasks/create-hashes.js
+++ gulpfile.js/tasks/create-hashes.js
@@ -1,31 +1,16 @@
 const requireCached = require('../src/gulp/require-cached')
 const config = require('../config')
-const merge = require('merge-stream')
 
 const path = requireCached('path')
 const gulp = requireCached('gulp')
-const through = requireCached('through2')
-const del = requireCached('del')
 const rev = require('gulp-rev')
-const revCollector = require('gulp-rev-collector')
-
-const removeOriginalFiles = through.obj(function(file, enc, cb) {
-  const manifest = JSON.parse(file.contents.toString(enc))
-  const filesToDelete = Object.keys(manifest).map(key =>
-    path.resolve(config.projectDirectory, config.dest.getPath('root'), key),
-  )
-  filesToDelete.push(file.path)
-  return del(filesToDelete, { force: true })
-    .then(filesDeleted => cb(null, filesDeleted))
-    .catch(err => cb(err))
-})
 
 gulp.task('create-hashes', function(callback) {
   const basePath = path.resolve(config.projectDirectory, config.dest.getPath('root'))
 
   if (!config.buildStatic) return callback.call(this)
 
-  const renamer = gulp
+  return gulp
     .src(
       [
         path.resolve(config.projectDirectory, config.dest.getPath('javascript', '*.js')),
@@ -37,22 +22,6 @@ gulp.task('create-hashes', function(callback) {
     )
     .pipe(rev())
     .pipe(gulp.dest(basePath))
-    .pipe(
-      rev.manifest({
-        path: 'rev-manifest.json',
-      }),
-    )
+    .pipe(rev.manifest())
     .pipe(gulp.dest(basePath))
-    .pipe(removeOriginalFiles)
-
-  const updateHtml = gulp
-    .src([
-      path.resolve(config.projectDirectory, config.dest.getPath('root', 'rev-manifest.json')),
-      path.resolve(config.projectDirectory, config.dest.getPath('html', '*.html')),
-      path.resolve(config.projectDirectory, config.dest.getPath('sw', 'sw.js')),
-    ])
-    .pipe(revCollector())
-    .pipe(gulp.dest(path.resolve(config.projectDirectory, config.dest.getPath('html'))))
-
-  return merge(renamer, updateHtml)
 })
---
+++ gulpfile.js/tasks/update-html-references.js
@@ -0,0 +1,40 @@
+const requireCached = require('../src/gulp/require-cached')
+const config = require('../config')
+
+const path = requireCached('path')
+const gulp = requireCached('gulp')
+const through = requireCached('through2')
+const del = requireCached('del')
+const merge = require('merge2')
+const revCollector = require('gulp-rev-collector')
+
+const removeOriginalFiles = () =>
+  through.obj(function(file, enc, cb) {
+    const manifest = JSON.parse(file.contents.toString(enc))
+    const filesToDelete = Object.keys(manifest).map(key =>
+      path.resolve(config.projectDirectory, config.dest.getPath('root'), key),
+    )
+    filesToDelete.push(file.path)
+    return del(filesToDelete, { force: true })
+      .then(filesDeleted => cb(null, filesDeleted))
+      .catch(err => cb(err))
+  })
+
+gulp.task('update-html-references', function(callback) {
+  if (!config.buildStatic) return callback.call(this)
+
+  const updateHtml = gulp
+    .src([
+      path.resolve(config.projectDirectory, config.dest.getPath('root', 'rev-manifest.json')),
+      path.resolve(config.projectDirectory, config.dest.getPath('html', '*.html')),
+      path.resolve(config.projectDirectory, config.dest.getPath('sw', 'sw.js')),
+    ])
+    .pipe(revCollector())
+    .pipe(gulp.dest(path.resolve(config.projectDirectory, config.dest.getPath('html'))))
+
+  const removeManifest = gulp
+    .src(path.resolve(config.projectDirectory, config.dest.getPath('root', 'rev-manifest.json')))
+    .pipe(removeOriginalFiles())
+
+  return merge(updateHtml, removeManifest)
+})
