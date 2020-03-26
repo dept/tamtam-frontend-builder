58a3009c6ab6b8f6b9f97bd13de08d4363621dcd
Adrian Klingen <adrian@tamtam.nl>
2020-03-26 22:42:15 +0100

    Add remove functionality

--- gulpfile.js/tasks/create-hashes.js
+++ gulpfile.js/tasks/create-hashes.js
@@ -4,9 +4,21 @@ const merge = require('merge-stream')
 
 const path = requireCached('path')
 const gulp = requireCached('gulp')
-const gulpif = requireCached('gulp-if')
+const through = requireCached('through2')
+const del = requireCached('del')
 const rev = require('gulp-rev')
 const revCollector = require('gulp-rev-collector')
+
+const removeOriginalFiles = through.obj(function(file, enc, cb) {
+  const manifest = JSON.parse(file.contents.toString(enc))
+  const filesToDelete = Object.keys(manifest).map(key =>
+    path.resolve(config.projectDirectory, config.dest.getPath('root'), key),
+  )
+  filesToDelete.push(file.path)
+  return del(filesToDelete, { force: true })
+    .then(filesDeleted => cb(null, filesDeleted))
+    .catch(err => cb(err))
+})
 
 gulp.task('create-hashes', function(callback) {
   const basePath = path.resolve(config.projectDirectory, config.dest.getPath('root'))
@@ -31,6 +43,7 @@ gulp.task('create-hashes', function(callback) {
       }),
     )
     .pipe(gulp.dest(basePath))
+    .pipe(removeOriginalFiles)
 
   const updateHtml = gulp
     .src([
