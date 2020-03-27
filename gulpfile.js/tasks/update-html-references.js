d2c1f8a0424de96fd50e6ddc33144682f09f22bd
Adrian Klingen <adrian@tamtam.nl>
2020-03-27 15:24:52 +0100

    Fix check

--- gulpfile.js/tasks/update-html-references.js
+++ gulpfile.js/tasks/update-html-references.js
@@ -21,7 +21,7 @@ const removeOriginalFiles = () =>
   })
 
 gulp.task('update-html-references', function(callback) {
-  if (!config.buildStatic && !config.debug) return callback.call(this)
+  if (config.debug && !config.buildStatic) return callback.call(this)
 
   const updateHtml = gulp
     .src([
