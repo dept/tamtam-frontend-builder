e37d922d6694a24330fb7a6c5e4a94974b9d60a2
Adrian Klingen <adrian@tamtam.nl>
2020-03-31 12:47:48 +0200

    Add log error

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -2,6 +2,7 @@ const config = require('../config')
 const requireCached = require('../src/gulp/require-cached')
 const fs = require('fs')
 const gulp = requireCached('gulp')
+const log = require('../src/debug/log')
 
 const compilerPromise = require('./script/create-compiler-promise')
 const webpackConfigs = require('./script/webpack-config')
@@ -16,7 +17,10 @@ gulp.task('js', function(callback) {
   Promise.all(compilerPromise.create(compilerConfigs))
     .then(() => callback())
     .catch(e => {
-      console.warn('Error whilst compiling JS', e)
+      log.error({
+        sender: 'js',
+        message: 'Error whilst compiling JS',
+      })
       callback(e)
     })
 })
