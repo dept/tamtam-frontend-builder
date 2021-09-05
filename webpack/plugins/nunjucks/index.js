73a3e3f0066a22fca9e634caecdbe06164063714
Adrian Klingen <adrian.klingen@deptagency.com>
2021-09-05 20:12:26 +0200

    Fix svg minimize and watch

--- webpack/plugins/nunjucks/index.js
+++ webpack/plugins/nunjucks/index.js
@@ -44,7 +44,7 @@ const configureNunjucksPlugin = () => {
   environment.addFilter('defaults', defaultsFilter)
 
   return new NunjucksWebpackPlugin({
-    paths: [`${config.source}/**/*.html`, `${config.data}/**/*.json`],
+    paths: [`${config.source}/**/*.html`, `${config.data}/**/*.json`, `${config.assets}/**/*.svg`],
     configure: environment,
     templates: templates,
   })
