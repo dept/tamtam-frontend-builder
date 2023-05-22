cbd96543388bf4c0919ea0453ef8bc05e8dbdf9a
Adrian Klingen <adrian.klingen@deptagency.com>
2023-05-22 14:34:45 +0200

    Migrate to sharp because @squoosh/lib is deprecated

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,10 @@ module.exports = (api) => {
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object'
     ],
     presets: [
       '@babel/preset-typescript',
