99d8a82a30dd4c161c7d7304997672cc5ddf3280
Adrian Klingen <adrian.klingen@deptagency.com>
2023-05-22 13:28:04 +0200

    Add babel proposals

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const extendFilePath = resolveApp('babel.extend.js')
 const hasExtendFile = fs.existsSync(extendFilePath)
 
-module.exports = (api) => {
+module.exports = api => {
   api.cache(true)
 
   let options = {
@@ -13,6 +13,10 @@ module.exports = (api) => {
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
     ],
     presets: [
       '@babel/preset-typescript',
